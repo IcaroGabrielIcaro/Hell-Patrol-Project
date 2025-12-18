e20295fc9e22f19374699d0eb4823ed225fa67a2
Icaro Gabriel <icarogabrielpc05@gmail.com>
2025-12-18 16:23:41 -0300

    feat: adiciona limites da arena ao movimento do jogador

--- hell-patrol/server/entities/player.py
+++ hell-patrol/server/entities/player.py
@@ -1,5 +1,7 @@
 from shared.world import WORLD_WIDTH, WORLD_HEIGHT, PLAYER_SIZE
 
+SPEED = 5
+
 class Player:
     def __init__(self):
         self.size = PLAYER_SIZE
@@ -7,8 +9,18 @@ class Player:
         self.y = (WORLD_HEIGHT // 2) - (PLAYER_SIZE // 2)
 
     def move(self, dx, dy):
-        self.x += dx * 5
-        self.y += dy * 5
+        self.x += dx * SPEED
+        self.y += dy * SPEED
+
+        if self.x < 0:
+            self.x = 0
+        elif self.x > WORLD_WIDTH - PLAYER_SIZE:
+            self.x = WORLD_WIDTH - PLAYER_SIZE
+
+        if self.y < 0:
+            self.y = 0
+        elif self.y > WORLD_HEIGHT - PLAYER_SIZE:
+            self.y = WORLD_HEIGHT - PLAYER_SIZE
 
     def to_dict(self):
         return {
