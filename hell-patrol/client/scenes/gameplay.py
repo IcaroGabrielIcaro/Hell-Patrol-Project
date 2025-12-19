a228c4b8382f80eeaf2d4fcdcdfdab891a13153a
Icaro Gabriel <icarogabrielpc05@gmail.com>
2025-12-19 20:08:11 -0300

    feat: Adiciona mais elementos no mapa

--- hell-patrol/client/scenes/gameplay.py
+++ hell-patrol/client/scenes/gameplay.py
@@ -4,11 +4,11 @@ from client.core.camera import Camera
 from client.world.tilemap import TileMap
 
 class GameplayScene:
-    def __init__(self, screen_width, screen_height, tile_image, player_id):
+    def __init__(self, screen_width, screen_height, tiles, player_id):
         self.players = {}
         self.camera = Camera(screen_width, screen_height)
         self.local_player_id = player_id
-        self.tilemap = TileMap(tile_image)
+        self.tilemap = TileMap(tiles)
 
     def update_state(self, state):
         self.players = {
