eb73627d3a415912630358311716cd3c16d22f2b
Francisco <gabrielcostabessa10@gmail.com>
2026-01-19 21:43:52 -0300

    Primeiro Commit

--- hell-patrol/client/entities/player.py
+++
@@ -1,9 +0,0 @@
-import pygame
-
-class Player:
-    def __init__(self, x, y, size):
-        self.rect = pygame.Rect(x, y, size, size)
-
-    def draw(self, screen, camera):
-        screen_rect = camera.apply(self.rect)
-        pygame.draw.rect(screen, (200, 50, 50), screen_rect)
--- hell-patrol/server/main.py
+++
@@ -1,5 +0,0 @@
-from server.config import *
-from server.core.server import GameServer
-
-server = GameServer(HOST, PORT)
-server.start()
